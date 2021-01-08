b4f8bcf7738bc6b80cac80f7f2d20bc47663acea
Ryan Thorpe <rmthorpe@princeton.edu>
2021-01-08 13:45:07 -0500

    basic spin integration

--- host.py
+++ host.py
@@ -1,11 +1,19 @@
-from puzzle import Puzzle 
+from puzzle import Puzzle
+from wheel import Wheel
+import time
 import os
 
 def setup():
     string = input("Choose the secret puzzle: \n")
+    wheel = Wheel(10, ['million', 'wildcard', 'trip'])
     puz = Puzzle(string)
     os.system('cls')
     while not puz.solveCheck():
+        print('take a spin! [press s then enter to spin]')
+        while input() != 's': continue
+        print('spinning...')
+        time.sleep(3)
+        print(wheel.spin())
         g = input("Guess a letter or solve [type solve]: \n")
         if g != 'solve':
             puz.guess(g)
